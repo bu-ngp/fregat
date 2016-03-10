32ed78972dc1266f26dc4a9b55585af6c175cbb0
sysadmin <sysadmin@POL1-KAB104-3.mugp1.local>
2016-03-10 10:43:38 +0500

    yii2

--- config/db.php
+++ config/db.php
@@ -1,9 +1,14 @@
 <?php
 
+$file = file(dirname(__FILE__) . DIRECTORY_SEPARATOR . "db", FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+$dbname = $file[0];
+$dbuser = $file[1];
+$dbpassword = $file[2];
+
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=127.0.0.1;dbname=baseportal',
-    'username' => 'root',
-    'password' => '265463',
+    'dsn' => 'mysql:host=127.0.0.1;dbname='.$dbname,
+    'username' => $dbuser,
+    'password' => $dbpassword,
     'charset' => 'utf8',
 ];
