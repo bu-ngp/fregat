67bc6ec1b49ca245ea028c70f4c1ef7eb8a1d930
sysadmin <sysadmin@POL1-KAB104-3.mugp1.local>
2016-03-14 09:14:24 +0500

    yii2

--- config/db.php
+++ config/db.php
@@ -7,7 +7,7 @@ $dbpassword = $file[2];
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=127.0.0.1;dbname='.$dbname.';charset=UTF-8',
+    'dsn' => 'mysql:host=127.0.0.1;dbname='.$dbname.';charset=UTF8',
     'username' => $dbuser,
     'password' => $dbpassword,
     'charset' => 'utf8',
