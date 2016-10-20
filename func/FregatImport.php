657cdf9887232a5457a748a16cca5247a97ea5f6
VOVANCHO <w-karpov@mail.ru>
2016-10-20 10:03:41 +0500

    dev

--- func/FregatImport.php
+++ func/FregatImport.php
@@ -812,15 +812,6 @@ class FregatImport
         if (self::IsFileType(self::mat) || self::IsFileType(self::gu))
             $Mattraffic->mattraffic_forimport = 1;
 
-        var_dump('employee_id');
-        var_dump($employee_id);
-        var_dump('$Mattraffic->recordapply');
-        var_dump($Mattraffic->recordapply);
-        var_dump('$Mattraffic->diff_number');
-        var_dump($Mattraffic->diff_number);
-        var_dump('!$Mattraffic->isNewRecord');
-        var_dump(!$Mattraffic->isNewRecord);
-
         if (!$Mattraffic->isNewRecord && $Mattraffic->recordapply && ((self::IsFileType(self::mat) || self::IsFileType(self::gu)) && $Mattraffic->diff_number != '0' || self::IsFileType(self::os))) { // Если у материальной ценности найден сотрудник и запись актуальна
             // Разница в количестве (Количество из Excel - количество из БД)
             $diff_number = $Mattraffic->diff_number;
@@ -850,6 +841,13 @@ class FregatImport
 
             // Количество материальной ценности у разных сотрудников
             $mat_number = self::GetCountMaterialByID($Material->material_id);
+
+            var_dump('employee_id');
+            var_dump($employee_id);
+            var_dump('$mat_number');
+            var_dump($mat_number);
+            var_dump('self::$material_number_xls');
+            var_dump(self::$material_number_xls);
 
             if ($mat_number == '-1') // Если новая материальная ценность
                 $mat_number = self::$material_number_xls;
