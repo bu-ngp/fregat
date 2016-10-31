4b2f07c42183c9e1dc6ddba981bcdcf08900f2d9
VOVANCHO <w-karpov@mail.ru>
2016-10-31 16:13:42 +0500

    dev

--- config/test_db.php
+++ config/test_db.php
@@ -6,7 +6,7 @@ $db = [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=127.0.0.1;dbname=baseportal;charset=UTF8',
     'username' => 'root',
-    'password' => '265463',
+    'password' => '',
     'charset' => 'utf8',
     'enableSchemaCache' => true, // php C:\www\yii2test_ps\yii cache/flush-schema db
 ];
