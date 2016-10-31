b1d046758b33747b8e64caee830b949560bb6a39
VOVANCHO <w-karpov@mail.ru>
2016-10-31 15:37:17 +0500

    dev

--- tests/unit/ExampleTest.php
+++ tests/unit/ExampleTest.php
@@ -18,7 +18,7 @@ class ExampleTest extends Unit
     protected function _before()
     {
         $initString = 'Исаева Ирина Владимировна|Поликлиника профилактических осмотров|Санитарка|Паспорт гражданина Российской Федерации|67 06|882218|Отделением УФМС России по ХМАО-Югре в Нижневартовском районе|06.02.2005 0:00:00|628616, Ханты-Мансийский Автономный округ - Югра АО, , Нижневартовск г, , Интернациональная ул,70,А,180|I862002355555|127-164-258 12|833-222|#001234|Ж|01.10.1955 0:00:00|#40817810700000123456|Ф-Л ЗС ПАО "ХАНТЫ-МАНСИЙСКИЙ БАНК ОТКРЫТИЕ"||';
-        $this->parseObject = EmployeeParseFactory::employee($initString);
+        $this->parseObject = EmployeeParseFactory::employee($initString)->create();
     }
 
     protected function _after()
@@ -33,9 +33,9 @@ class ExampleTest extends Unit
         $this->assertTrue($this->parseObject->dolzh_name === 'Санитарка');
         $this->assertTrue($this->parseObject->podraz_name === 'Поликлиника профилактических осмотров');
         $this->assertTrue($this->parseObject->build_name === 'Поликлиника профилактических осмотров');
-        $this->assertTrue($this->parseObject->profile_dr === '01.10.1955');
+        $this->assertTrue($this->parseObject->profile_dr === '01.10.1955 0:00:00');
         $this->assertTrue($this->parseObject->profile_pol === 'Ж');
-        $this->assertTrue($this->parseObject->profile_inn === '862002355555');
+        $this->assertTrue($this->parseObject->profile_inn === 'I862002355555');
         $this->assertTrue($this->parseObject->profile_snils === '127-164-258 12');
         $this->assertTrue($this->parseObject->profile_address === '628616, Ханты-Мансийский Автономный округ - Югра АО, , Нижневартовск г, , Интернациональная ул,70,А,180');
     }
