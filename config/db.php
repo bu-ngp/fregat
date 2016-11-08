c2b5a1ba1ca3087d491822ab0b956fa8c399e74f
VOVANCHO <w-karpov@mail.ru>
2016-11-08 11:39:21 +0500

    dev

--- config/db.php
+++ config/db.php
@@ -12,7 +12,7 @@ return [
   //  'username' => $dbuser,
   //  'password' => $dbpassword,
     'username' => 'root',
-    'password' => '265463',
+    'password' => '',
     'charset' => 'utf8',
     'enableSchemaCache' => true, // php C:\www\yii2test_ps\yii cache/flush-schema db
 ];
