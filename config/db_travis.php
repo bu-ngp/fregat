082652d88f7f5acef6c4c28524ae7cf15df66a05
VOVANCHO <w-karpov@mail.ru>
2016-11-09 11:00:35 +0500

    dev

---
+++ config/db_travis.php
@@ -0,0 +1,9 @@
+<?php
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=127.0.0.1;dbname=baseportal;charset=UTF8',
+    'username' => 'root',
+    'password' => '265463',
+    'charset' => 'utf8',
+    'enableSchemaCache' => true, // php C:\www\yii2test_ps\yii cache/flush-schema db
+];
