0fec1b1f9b4f03bbcf8c43d416637b703c6196a7
VOVANCHO <w-karpov@mail.ru>
2016-11-14 09:53:03 +0500

    dev

--- tests/acceptance/LoginCest.php
+++ tests/acceptance/LoginCest.php
@@ -1,7 +1,7 @@
 <?php
 
 
-//use yii\helpers\Url;
+use yii\helpers\Url;
 
 /**
  * @group LoginCest
@@ -18,13 +18,13 @@ class LoginCest
 
     public function login(AcceptanceTester $I)
     {
-        /*  $I->see('Введите логин и пароль для входа в систему:');
+          $I->see('Введите логин и пароль для входа в систему:');
           $I->amOnPage(Url::toRoute('/site/login'));
           $I->see('Введите логин и пароль для входа в систему:');
           $I->fillField('LoginForm[username]', 'admin');
           $I->fillField('LoginForm[password]', 'admin');
           $I->click('login-button');
           $I->wait(2); // wait for button to be clicked
-          $I->see('Главное меню');*/
+          $I->see('Главное меню');
     }
 }
