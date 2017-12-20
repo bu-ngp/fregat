3062944d3021fb4771e993c7a21e905c2bef6a6b
VOVANCHO <w-karpov@mail.ru>
2017-12-20 15:20:56 +0500

    dev

--- controllers/Fregat/FregatController.php
+++ controllers/Fregat/FregatController.php
@@ -404,8 +404,8 @@ class FregatController extends Controller
         $filesFromZip = [];
 
         var_dump($zip->getNameIndex(0));
-       // var_dump(mb_convert_encoding($zip->getNameIndex(0), 'UTF-8', 'Windows-1252'));
-        var_dump(mb_detect_encoding($zip->getNameIndex(0)));
+        // var_dump(mb_convert_encoding($zip->getNameIndex(0), 'UTF-8', 'Windows-1252'));
+        var_dump(mb_detect_encoding($zip->getNameIndex(0), mb_detect_order(), true));
         /*   for ($i = 0; $i < $zip->numFiles; $i++) {
                $filesFromZip[] = mb_convert_encoding($zip->getNameIndex($i), 'UTF-8', 'CP866');
            }
